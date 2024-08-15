fc956e8bf9cd5c702411c10ff6aa508b132c7706
jadenn1 <jadenmumu@gmail.com>
2024-08-15 13:19:35 +1000

    added homescreen dashboard stuff

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,10 @@
 
 import { useState } from 'react';
 import Header from './components/Header';
+import HomePage from './pages/home-page';
 import Teams from './pages/team-page';
 import Projects from './pages/project-page';
 import Tasks from './pages/task-page';
-import Card from './components/Card'
-import TaskItem from './components/TaskItem'
 import styles from "./styles/Layout.module.css";
 
 export default function Home() {
@@ -16,7 +15,7 @@ export default function Home() {
     <>
       <Header setCurrentPage={setCurrentPage} />
       <main className={styles.main}>
-        {currentPage === '/' && <h1>Welcome to AssignMate</h1>}
+        {currentPage === '/' && <HomePage />}
         {currentPage === '/teams' && <Teams />}
         {currentPage === '/projects' && <Projects />}
         {currentPage === '/tasks' && <Tasks />}
--- app/pages/project-page.tsx
+++ app/pages/project-page.tsx
@@ -1,19 +1,40 @@
-import TeamCard from '../components/Card'
+"use client" // Renders this component on the client/frontend, not just on the server/backend.
+
+import React, { useState } from 'react';
+import Card from '../components/Card';
+import SidePanel from '../components/SidePanel';
 import styles from '../styles/page.module.css'
 
+interface Project {
+  name: string;
+  color: string;
+}
+
 export default function Projects() {
-  const teams = [
+
+  const [selectedProject, setSelectedProject] = useState<Project | null>(null);
+
+  const projects = [
     { name: 'Project Alpha', color: 'blue' },
     { name: 'Project Beta', color: '#ff99ff' },
     { name: 'Project Gamma', color: 'red' },
     { name: 'Project Delta', color: 'green' }
   ]
 
+  const handleCardClick = (team: Project) => {
+    setSelectedProject(team);
+  };
+
+  const handleCloseSidePanel = () => {
+    setSelectedProject(null);
+  };
+
   return (
     <div className={styles.grid}>
-      {teams.map(team => (
-        <TeamCard key={team.name} name={team.name} color={team.color} />
+      {projects.map(project => (
+        <Card key={project.name} name={project.name} color={project.color} onClick={() => handleCardClick(project)} />
       ))}
+      <SidePanel team={selectedProject} onClose={handleCloseSidePanel} />
     </div>
   )
 }
