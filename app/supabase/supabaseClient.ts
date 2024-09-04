478ef804a6b4b704ab18408396762b7f505124c4
Carl Perera <carl7perera@gmail.com>
2024-09-04 21:28:45 +1000

    rechanged enviro file

--- app/supabase/supabaseClient.ts
+++ app/supabase/supabaseClient.ts
@@ -7,7 +7,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Supabase URL and Key are required');
+  throw new Error(`Supabase URL and Key are required: ${process.env.NODE_ENV}`);
 }
 
 
