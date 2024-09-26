dc7a07210833b20312edd5c51adb4de5bff85322
Patrick <patrick.fitzgerald29@gmail.com>
2024-09-26 11:51:19 +0930

    adjusted header padding

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <header className="bg-white">
-          <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <Link href="/home-page">
               <h1 className="text-2xl font-bold text-gray-900">AssignMate</h1>
             </Link>
